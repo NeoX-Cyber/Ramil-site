e1bd58865274c2002c6d91d5b72fb10129279faf
unknown <ramil.mhrmf@gmail.com>
2026-02-11 00:42:53 +0400

    İlk yükləmə

---
+++ matrix.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const letters = "01";
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+const drops = Array(Math.floor(columns)).fill(1);
+
+function drawMatrix() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#00ff66";
+  ctx.font = fontSize + "px monospace";
+
+  drops.forEach((y, i) => {
+    const text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillText(text, i * fontSize, y * fontSize);
+
+    if (y * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  });
+}
+
+setInterval(drawMatrix, 50);
+
+/* LOADER TIME */
+setTimeout(() => {
+  document.querySelector(".loader-text").textContent = "ACCESS GRANTED";
+}, 2000);
+
+setTimeout(() => {
+  document.getElementById("loader").style.display = "none";
+}, 3000);
